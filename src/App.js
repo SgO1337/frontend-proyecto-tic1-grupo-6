840c91bb58ceaa8c578dbd46759d4723f4fcb42d
ExoSnipazz <rodrigoquincke1@gmail.com>
2024-10-03 13:45:06 -0300

    funciona

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import UserTable from './components/UserTable';
 import UserForm from './components/UserForm';
 import HomePage from './pages/HomePage';
-import LoginPage from './components/LoginPage';
-import SignUpPage from './components/SignUpPage';
-import SelectionInfoPage from "./components/SelectionInfoPage";
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import SelectionInfoPage from "./pages/SelectionInfoPage";
 
 
 const App = () => {
